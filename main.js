7d0dd7a03d4671e8197802ca8b6f02b18fa0e308
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2020-12-25 19:41:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,24 @@ var pictures=[
     "family book.jpg",
     "mom.jpg",
     "dad.jpg",
-    "son.png"
+    "son.png",
+    "grandmother.jpg",
+    "grandfather.png"
   ];
   var labels=[
       "Family Book",
     "Seethalakshmi Kannan",
     "Rengaraman Govindan",
-    "Surya Renga Raman"
+    "Surya Renga Raman",
+    "Padmavathy Kannan",
+    "Kannan"
 ];
   var i = 0;
   function nextpic(){
   document.getElementById("family_pictures").src=pictures[i];
   document.getElementById("family_names").innerHTML=labels[i];
    i++;
-    if(i==4){
+    if(i==6){
       i=0;
   }
   
